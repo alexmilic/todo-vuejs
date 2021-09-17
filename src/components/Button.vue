7e908396b95729cf18484b516d6e430ebcce6bae
Aleksandar Milic <alexmilic01@gmail.com>
2021-09-17 19:56:46 +0200

    Add components

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button 
+        :style="{backgroundColor: color}"
+        class="btn"
+    >
+        {{ text }}
+    </button>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String,
+            default: 'Add task'
+        },
+        color: {
+            type: String,
+            color: "#ccc"
+        }
+    }
+}
+</script>
